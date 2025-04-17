f18ab53475a6478f2f78def46e32b018bf0cf744
Sharonah8 <nyamongosharon88@gmail.com>
2025-04-17 14:01:20 +0300

    Creating a pet object and calling its methods to test functionality

---
+++ main.py
@@ -0,0 +1,5 @@
+from pet import Pet; 
+
+my_pet = Pet("Shazzy")
+print(f"I created a pet called {my_pet.name}")
+print(f"Hunger: {my_pet.hunger}, Energy: {my_pet.energy}, Happiness: {my_pet.happiness}")
