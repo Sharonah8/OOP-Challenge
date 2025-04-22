1b3cda4defb356d385cebc00cc44df94a8a46100
Sharonah8 <nyamongosharon88@gmail.com>
2025-04-22 09:54:35 +0300

    Create sleep  method

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from pet import Pet;
 my_pet = Pet("Shazzy")
 print(f"Creating pet: {my_pet.name}")
 my_pet.eat()
+my_pet.sleep()
 print(f"Hunger: {my_pet.hunger}, Energy: {my_pet.energy}, Happiness: {my_pet.happiness}")
 
--- pet.py
+++ pet.py
@@ -10,3 +10,7 @@ class Pet:
         self.hunger = max(0, self.hunger - 3)
         self.happiness = min(10, self.happiness + 1)
         print(f"{self.name} is eating...")
+
+    def sleep(self):
+        self.energy = min(10, self.energy + 5)
+        print(f"{self.name} is sleeping...")
