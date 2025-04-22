568b65133c4ddecee88dfa5b59dd70ce0eaa0956
Sharonah8 <nyamongosharon88@gmail.com>
2025-04-22 09:52:17 +0300

    Create eat method

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pet import Pet; 
 
 my_pet = Pet("Shazzy")
-print(f"I created a pet called {my_pet.name}")
+print(f"Creating pet: {my_pet.name}")
+my_pet.eat()
 print(f"Hunger: {my_pet.hunger}, Energy: {my_pet.energy}, Happiness: {my_pet.happiness}")
+
--- pet.py
+++ pet.py
@@ -5,3 +5,8 @@ class Pet:
         self.energy = 5
         self.happiness = 5
         self.tricks = []
+
+    def eat(self):
+        self.hunger = max(0, self.hunger - 3)
+        self.happiness = min(10, self.happiness + 1)
+        print(f"{self.name} is eating...")
